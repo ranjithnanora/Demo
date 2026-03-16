f85bd9294f3839dcb98d1d9be64d8e6d6bbea76b
ANORA\ranjith.n <Anora>
2026-03-16 12:43:07 +0530

    added Reverse vowels in array logic and also modified consecutive_numbers into function

--- DSA.py
+++ DSA.py
@@ -1,27 +1,54 @@
+#sum consecutive numbers
 from collections import defaultdict
-arr=[1,1,2,5,3,3,6,12]
+def sum_consecutive_numbers(arr):
+    mp = defaultdict(int)
+    for ele in arr:
+        mp[ele] += 1
 
-mp=defaultdict(int)
-for ele in arr:
-    mp[ele]+=1
+    change = True
+    while change:
+        change = False
+        new_mp = defaultdict(int)
+        for key, value in mp.items():
+            if value > 1:
+                new_mp[key * value] += 1
+                change = True
+            else:
+                new_mp[key] += value
 
-change=True
-while change:
-    change=False
-    new_mp=defaultdict(int)
-    for key,value in mp.items():
-        if value>1:
-            new_mp[key*value]+=1
-            change=True
-        else:
-            new_mp[key]+=value
+        mp = new_mp.copy()
 
-    mp=new_mp.copy()
+    result = []
+    for key in mp.keys():
+        result.append(key)
 
-result=[]
-for key in mp.keys():
-    result.append(key)
+    return result
 
-print(result)
+print(sum_consecutive_numbers([1,1,2,5,3,3,6,12]))
+print(sum_consecutive_numbers([1,1,3,3,5,6,7]))
+
+
+#reverse vowels
+def reverse_vowels(word):
+    word_arr = list(word)
+    n=len(word)
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    i = 0;
+    j = len(word) - 1
+    while i < j:
+        while i < n and word_arr[i] not in vowels:
+            i += 1
+        while j >= 0 and word_arr[j] not in vowels:
+            j -= 1
+        temp = word_arr[i]
+        word_arr[i] = word_arr[j]
+        word_arr[j] = temp
+        i += 1
+        j -= 1
+    return "".join(word_arr)
+
+
+print(reverse_vowels("icecream")) #acecreim
+print(reverse_vowels("elephant")) #alephent
 
 
